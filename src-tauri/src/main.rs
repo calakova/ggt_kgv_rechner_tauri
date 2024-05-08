a760b5195c203ae02e32d986ff54090d8ea5d9bb
Christoph Wiesenhofer <wiesenhoferc@gmail.com>
2024-05-08 12:39:48 +0200

    Add error handling for overflow

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -10,30 +10,54 @@ fn calc(num1: &str, num2: &str) -> String {
     let b = parse_input_to_int(read_number_input(num2)).abs();
 
     let ggt = euclid(a, b);
-    let kgv = (a * b) / ggt;
+    let kgv = kgv_calc(a, b, ggt);
 
     format!("ggT: {}, kgV: {}", ggt, kgv)
 }
 
-fn read_number_input(input: &str) -> Result<i32, ParseIntError> {
-    let number: i32 = input.trim().parse()?;
+fn read_number_input(input: &str) -> Result<i64, ParseIntError> {
+    let number: i64 = input.trim().parse()?;
     Ok(number)
 }
 
-fn parse_input_to_int(result: Result<i32, ParseIntError>) -> i32 {
+fn parse_input_to_int(result: Result<i64, ParseIntError>) -> i64 {
     match result {
         Ok(num) => num,
         Err(_) => -1
     }
 }
 
-fn euclid(mut a: i32, mut b: i32) -> i32 {
+fn euclid(mut a: i64, mut b: i64) -> i64 {
     while b != 0 {
         let h = a % b;
         a = b;
         b = h;
     }
     a
+}
+
+fn kgv_calc(a: i64, b: i64, ggt: i64) -> String {
+    let error_str = "Ergebnis zu groß".to_string();
+    
+    let product = match a.checked_mul(b) {
+        Some(n) => n,
+        None => {
+            -1
+        }
+    };
+
+    if product == -1 {
+        return error_str
+    }
+
+    let kgv = match product.checked_div(ggt) {
+        Some(n) => n.to_string(),
+        None => {
+            error_str
+        }
+    };
+    
+    kgv
 }
 
 fn main() {
