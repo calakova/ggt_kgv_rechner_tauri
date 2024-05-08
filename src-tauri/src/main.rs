0ddc6b1da45a5f742d2e3761a71cbdaac53d0868
Christoph Wiesenhofer <wiesenhoferc@gmail.com>
2024-05-08 12:44:12 +0200

    Change i64 to u64

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,8 +6,8 @@ use std::num::ParseIntError;
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn calc(num1: &str, num2: &str) -> String {
-    let a = parse_input_to_int(read_number_input(num1)).abs();
-    let b = parse_input_to_int(read_number_input(num2)).abs();
+    let a = parse_input_to_int(read_number_input(num1));
+    let b = parse_input_to_int(read_number_input(num2));
 
     let ggt = euclid(a, b);
     let kgv = kgv_calc(a, b, ggt);
@@ -15,19 +15,19 @@ fn calc(num1: &str, num2: &str) -> String {
     format!("ggT: {}, kgV: {}", ggt, kgv)
 }
 
-fn read_number_input(input: &str) -> Result<i64, ParseIntError> {
-    let number: i64 = input.trim().parse()?;
+fn read_number_input(input: &str) -> Result<u64, ParseIntError> {
+    let number: u64 = input.trim().parse()?;
     Ok(number)
 }
 
-fn parse_input_to_int(result: Result<i64, ParseIntError>) -> i64 {
+fn parse_input_to_int(result: Result<u64, ParseIntError>) -> u64 {
     match result {
         Ok(num) => num,
-        Err(_) => -1
+        Err(_) => 0
     }
 }
 
-fn euclid(mut a: i64, mut b: i64) -> i64 {
+fn euclid(mut a: u64, mut b: u64) -> u64 {
     while b != 0 {
         let h = a % b;
         a = b;
@@ -36,17 +36,17 @@ fn euclid(mut a: i64, mut b: i64) -> i64 {
     a
 }
 
-fn kgv_calc(a: i64, b: i64, ggt: i64) -> String {
+fn kgv_calc(a: u64, b: u64, ggt: u64) -> String {
     let error_str = "Ergebnis zu groß".to_string();
-    
+
     let product = match a.checked_mul(b) {
         Some(n) => n,
         None => {
-            -1
+            0
         }
     };
 
-    if product == -1 {
+    if product == 0 {
         return error_str
     }
 
